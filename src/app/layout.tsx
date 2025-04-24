79b80f280b4508df430d479d22e0cc6b6c72d82a
Avinash Sah <121078730+avinashsah995@users.noreply.github.com>
2025-04-25 00:04:16 +0530

    feat(ui):  Ant design Install, Folder based routing setup, middleware file created for authentication of users (#3)
    this commit Introduces   Ant design Install, Folder based routing setup, middleware file created for authentication of users
    
    Authored-by: Avinash Sah <avinashsah995>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import 'antd/dist/reset.css';
 import { ThemeProvider } from "@/context/ThemeContext";
 
 const geistSans = Geist({
