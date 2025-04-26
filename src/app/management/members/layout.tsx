326c070525328a4830615cfa1d996a34e4820b24
avinashsah995 <avinashsah5555@gmail.com>
2025-04-26 20:05:46 +0530

    feat(ui):  Members Layout
    this commit Introduces  Members Layout
    
    Authored-by: Avinash Sah <avinashsah995>

--- src/app/management/members/layout.tsx
+++ src/app/management/members/layout.tsx
@@ -1,11 +1,16 @@
+'use client'
+import Tabs from '@/components/Tabs';
 import React from 'react'
 
-const layout = () => {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div>
-      memberslayout
-    </div>
+    <main className="w-full flex flex-col gap-2 flex-1">
+      <Tabs />
+      {children}
+    </main>
   )
 }
-
-export default layout
---
+++ src/components/formComponents/FormInputSearch.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Input, Button } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import Image from 'next/image';
+
+interface CustomSearchProps {
+  placeholder?: string;
+  onSearch?: (value: string) => void;
+  disable?: boolean;
+  customClass?: string;
+  searchInput?: string;
+  setSearchInput?: (value: string) => void;
+}
+
+const FormInputSearch: React.FC<CustomSearchProps> = ({
+  placeholder = 'Search...',
+  onSearch,
+  disable = false,
+  customClass,
+  searchInput,
+  setSearchInput
+}) => {
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchedValue = e.target.value;
+
+    if (setSearchInput) {
+      setSearchInput(searchedValue);
+    }
+  };
+ 
+  return (
+    <div className={`flex items-end w-full relative ${customClass}`}>
+      <Input
+        id="custom-search-input"
+        placeholder={placeholder}
+        disabled={disable}
+        onChange={handleSearch}
+        value={searchInput}
+        className="!h-[40px] !px-3 !py-1.5 !rounded-lg !border !border-[#D5D8D8] !bg-[#FFF] !shadow-[0px_1px_2px_0px_rgba(184,200,224,0.22)] box-border !text-[#032F35] !font-sans !text-[16px] !font-medium !leading-6 !w-full"
+      />
+    </div>
+  );
+};
+
+export default FormInputSearch;
