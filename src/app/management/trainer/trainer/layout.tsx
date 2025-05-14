71f5df3ac1628aefd6374ed6888e3a9629592ba6
avinashsah995 <avinashsah5555@gmail.com>
2025-05-14 16:41:05 +0530

    layout fix

--- src/app/management/trainer/trainer/layout.tsx
+++ src/app/management/trainer/trainer/layout.tsx
@@ -1,8 +1,5 @@
 'use client'
 import Trainer from '@/allPages/trainer/trainer';
-import Tabs from '@/components/Tabs';
-import { trainerTabs } from '@/constant/tabsData';
-import { useParams } from 'next/navigation';
 import React from 'react'
 
 export default function RootLayout({
